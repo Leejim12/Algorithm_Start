1918ad3b30182375d98962da9ac4f3ac79b00143
JimLee12 <qkq010177@gmail.com>
2022-10-21 14:12:04 +0900

    22.10.21

---
+++ IntArrayInit.java
@@ -0,0 +1,12 @@
+package 깃.PNU_Algorithm;
+
+public class IntArrayInit {
+
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4,5};
+		
+		for (int i = 0; i< a.length; i ++)
+			System.out.println("a[" + i + "] = " + a[i]);
+	}
+
+}
---
+++ ReverseArray.java
@@ -0,0 +1,40 @@
+package 깃.PNU_Algorithm;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class ReverseArray {
+	
+	static void swap(int[] a,int idx1, int idx2) {
+		int t = a[idx1]; a[idx1] = a[idx2]; a[idx2] = t;
+	}
+	
+	static void reverse(int[] a) {
+		for(int i = 0;i<a.length/2;i++)
+			swap(a,i,a.length-i-1);
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("total index number : ");
+		int num = stdIn.nextInt();
+		
+		int[] x = new int[num];
+		
+		for(int i = 0;i<num;i++) {
+			System.out.print("x[" + i + "]: ");
+			x[i] = stdIn.nextInt();
+		}
+		
+		System.out.println("x = " + Arrays.toString(x));
+		
+		reverse(x);
+		
+		System.out.println("Reverse arrangement.");
+		System.out.println("x = " + Arrays.toString(x));
+		
+	}
+
+}
